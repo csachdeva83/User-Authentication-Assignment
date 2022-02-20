36b595b0ac4ad28998467a79378774d22663a7a0
Cherish <cherishsachdeva16@gmail.com>
2022-02-20 17:50:28 +0530

    Adding Google OAuth

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,6 +5,10 @@ import {useNavigate} from "react-router-dom";
 
 const Login = ({setLoginUser})=>{
 
+    const google = ()=>{
+        window.open("http://localhost:4500/auth/google","_self")
+    }
+
     const navigate=useNavigate();
 
     const [user,setUser]=useState({
@@ -45,6 +49,11 @@ const Login = ({setLoginUser})=>{
             <div className="button" onClick={login}>Login</div>
             <div>or</div>
             <div className="button" onClick={()=>navigate("/register")}>Register</div>
+            <div>or</div>
+            <div className="loginButton google" onClick={google}>
+                <img src="../img/google.png" alt="" className="icon" />
+                Google
+            </div>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+const express=require("express");
+const cors= require("cors");
+const mongoose = require('mongoose');
+const cookieSession=require('cookie-session');
+const passport=require('passport');
+const passportSetup=require('./passport');
+const authRoute=require("./routes/auth");
 
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000/",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
+
+app.use(cookieSession({
+    name:"session",
+    keys:["assignment"],
+    maxAge: 24*60*60*100    
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userAuthentication",{
     useNewUrlParser: true,
@@ -61,5 +78,7 @@ app.post('/register',(req,res)=>{
         }
     })
 })
+
+app.use("/auth",authRoute);
 
 app.listen(4500,()=> console.log("Server started at 4500"));
